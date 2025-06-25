9511d6c327232a043dc712a58c9921e3af12e454
Lohtharr <manuel@oelmaier.eu>
2025-06-25 18:20:07 +0200

    added chat

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,66 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { request } from 'http';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "test-driven-llm" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('test-driven-llm.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Test-driven-LLM!');
-	});
 
-	context.subscriptions.push(disposable);
+	// The commandId parameter must match the command field in package.json
+	context.subscriptions.push(
+		vscode.commands.registerCommand('test-driven-llm.helloWorld', () => {
+
+		vscode.window.showInformationMessage('Hello World from Test-driven-LLM!');
+		})
+	);
+
+	//TODO: add python server and communication with it.
+	//TODO: add function to write the code somwhere in the editor.
+	//TODO: open chat command.
+	
+
+	context.subscriptions.push(
+			 vscode.commands.registerCommand('test-driven-llm.detectTests',() => {
+		const testController = vscode.tests.createTestController("test","TestController");
+	})
+
+	);
+		
+
+		const handler: vscode.ChatRequestHandler = async (
+		request: vscode.ChatRequest,
+		context: vscode.ChatContext,
+		stream: vscode.ChatResponseStream,
+		token: vscode.CancellationToken
+		) => {
+		switch (request.command) {
+			case "detectTests":
+				stream.markdown("Comming soon");
+			case "generateCodeFromDescription":
+				stream.markdown("Comming soon");
+			case "generateCodeFromTests":
+				stream.markdown("Comming soon");
+			case "help":
+				stream.markdown(` Here are some commands you can use:  
+				- help : Show this help message  
+				- detectTests : Detect tests in the current workspace  
+				- generateCodeFromDescription : Generate code from a description  
+				- generateCodeFromTests : Generates python code that passes the Tests  
+				`);		
+
+		}
+		return;
+	};
+
+
+
+	const LLMChat = vscode.chat.createChatParticipant('berlin-llm', handler);
+	LLMChat.iconPath = vscode.Uri.joinPath(context.extensionUri,  'LLM.jpeg');
+
+
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
