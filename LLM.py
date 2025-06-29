203980850706c966f2bf96fbbeba6657ce4b7a76
Lohtharr <manuel@oelmaier.eu>
2025-06-29 18:54:14 +0200

    added shutting down / staring LLM as commands

--- LLM.py
+++ LLM.py
@@ -1,17 +1,46 @@
 # llm_server.py
-import sys
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import sys,torch
+from transformers import AutoModelForCausalLM, AutoTokenizer,pipeline,BitsAndBytesConfig
 
-url = "bigcode/starcoderbase-1b"
+url = "/home/focus/.cache/huggingface/hub/models--bigcode--starcoderbase-1b/snapshots/182f0165fdf8da9c9935901eec65c94337f01c11"
 
-model = AutoModelForCausalLM.from_pretrained(url)
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+model = AutoModelForCausalLM.from_pretrained(
+    url,    
+    quantization_config=bnb_config,
+    device_map="auto",
+)
+
 tokenizer = AutoTokenizer.from_pretrained(url)
+
+
+tokenizer.pad_token = tokenizer.eos_token
+model.config.pad_token_id = tokenizer.pad_token_id
+
+text_generator = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens = 256,
+)
+
+def generate_response(prompt:str) -> str:
+    response = text_generator(prompt)
+    gen_text = response[0]["generated_text"]
+    return gen_text
+
+print("LLM is ready to receive input.", flush=True)
+
 
 for line in sys.stdin:
     try:
-        inputs = tokenizer(line, return_tensors="pt")
-        outputs = model.generate(**inputs)
-        response = {tokenizer.decode(outputs[0], skip_special_tokens=True)}
+        response = generate_response(line.strip())
         print(response, flush=True)
     except Exception as e:
         print({"error": str(e)}, flush=True,file=sys.stderr)
--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,30 @@
 import { spawn } from 'child_process';
+import { assert } from 'console';
 import * as vscode from 'vscode';
 
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "test-driven-llm" is now active!');
+	console.log('Congratulations, your extension "LocalPythonCodingLLM" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 
 	// The commandId parameter must match the command field in package.json
 	context.subscriptions.push(
-		vscode.commands.registerCommand('test-driven-llm.helloWorld', () => {
+		vscode.commands.registerCommand('LocalPythonCodingLLM.helloWorld', () => {
 
-		vscode.window.showInformationMessage('Hello World from Test-driven-LLM!');
+		vscode.window.showInformationMessage('Hello World from LocalPythonCodingLLM');
 		})
 	);
 
 	//TODO: add python server and communication with it.
 	//TODO: add function to write the code somwhere in the editor.
 	//TODO: open chat command.
-	
+	let LLM = startLLM();
+	let LLMready = false;
 
 	context.subscriptions.push(
-			 vscode.commands.registerCommand('test-driven-llm.detectTests',() => {
+			 vscode.commands.registerCommand('LocalPythonCodingLLM.detectTests',() => {
 		const testController = vscode.tests.createTestController("test","TestController");
 	})
 
@@ -39,57 +41,82 @@ export function activate(context: vscode.ExtensionContext) {
 			case "detectTests":
 				stream.markdown("Comming soon");
 				break;
-			case "generateCodeFromDescription":
-			case "generateCodeFromTests":
+			case "queryLLM":
+				if(!LLMready){
+					stream.markdown("Starting the LLM...\n");
+					LLM = startLLM();
+					stream.markdown("LLM started with PID: " + LLM.pid+ "\n");
+				}
 				const response = await queryLLM(request.prompt);
 				stream.markdown(response);
 				break;
+			case "startLLM":
+				stream.markdown("Starting the LLM...\n");
+				stream.markdown("LLM started with PID: " + LLM.pid+ "\n");
+				break;
+			case "stopLLM":
+				stream.markdown("Stopping the LLM...");
+				LLM.kill();
+				LLMready = false;
+				stream.markdown("LLM stopped.");
+				break;
+			default:
 			case "help":
-				stream.markdown(` Here are some commands you can use:  
-				- help : Show this help message  
-				- detectTests : Detect tests in the current workspace  
-				- generateCodeFromDescription : Generate code from a description  
-				- generateCodeFromTests : Generates python code that passes the Tests  
-				`);
-				break;		
+			stream.markdown(` Here are some commands you can use:  
+			- help : Show this help message  
+			- detectTests : Detect tests in the current workspace  
+			- queryLLM : query the LLM with a prompt  
+			- startLLM : Load the LLM and get it ready for use
+			- stopLLM : Stop the LLM process and clean up resources
+			`);			
 
 		}
 		return;
 	};
 
 
-
-	const LLMChat = vscode.chat.createChatParticipant('berlin-llm', handler);
+	const LLMChat = vscode.chat.createChatParticipant('LocalPythonCodingLLM', handler);
 	LLMChat.iconPath = vscode.Uri.joinPath(context.extensionUri,  'LLM.jpeg');
 
-	const LLMpath = vscode.Uri.joinPath(context.extensionUri, 'LLM.py').fsPath;
-	const PythonvenvPath = vscode.Uri.joinPath(context.extensionUri, '.venv/bin/python').fsPath;
-	const LLM = spawn(PythonvenvPath, [LLMpath]);
 
-	// toDO create a virtual environment and install the requirements.
+		// TODO: get python version with correct installs
 
-	console.log(`LLM PID: ${LLM.pid}`);
 
-		LLM.stderr.on('data', (data) => {
+	
+
+	 function startLLM() {
+
+		const LLMpath = vscode.Uri.joinPath(context.extensionUri, 'LLM.py').fsPath;
+		const PythonvenvPath = vscode.Uri.joinPath(context.extensionUri, '.venv/bin/python').fsPath;
+
+		const llm = spawn(PythonvenvPath, [LLMpath]);
+		llm.stderr.on('data', (data) => {
 			console.error(`LLM Error: ${data}`);
 		});
 
-		LLM.on('close', (code) => {
+		llm.on('close', (code) => {
 			console.log(`LLM process exited with code ${code}`);
 		});
+		llm.stdout.once('data', (data) => {
+			LLMready = data.includes("LLM is ready to receive input.");
+		});
+		return llm;
+
+	}
 
 	async function queryLLM(prompt: string): Promise<string> {
-		console.log('Received prompt:', prompt);
+		console.log('sending prompt:', prompt);
 
-		LLM.stdin.write(JSON.stringify(prompt) + '\n');
+		LLM.stdin.write(prompt + '\n');
 		return new Promise((resolve, reject) => {
 			LLM.stdout.once('data', (data) => {
 				const response = data.toString().trim();
+				console.log('received response:', response);
 				resolve(response);
 			});
 
 			LLM.stderr.once('data', (error) => {
-				reject(error.toString());
+				reject("An error occurred: " + error.toString());
 			});
 		});		
 	}
